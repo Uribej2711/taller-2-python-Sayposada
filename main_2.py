e4cbf7dec76a1ef6249ad313b257ba9f8fe72f73
Sayposada <siposadaa@unal.edu.co>
2024-02-19 23:27:25 -0500

    segunda oportunidad

---
+++ main_2.py
@@ -0,0 +1,51 @@
+class Auto:
+    
+    cantidadCreados = 0
+    
+    def _init_ (self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        
+        
+    def cantidadAsientos (self):
+        numAsientos = 0
+        for asiento in self.asientos:
+            if (type(asiento) == Asiento):
+                numAsientos += 1
+        return numAsientos
+
+    def verificarIntegridad (self):
+        if(self.registro == self.motor.registro):  
+            for asiento in self.asientos:
+                if (type(asiento) == Asiento):
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"                
+class Motor:
+    
+    def _init_ (self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo (self, tipo):
+        if(tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo       
+            
+class Asiento:
+    
+    def _init_ (self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if (color =="rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco"):
+            self.color = color
